4cdd0d169343fd0344a70c0c11a0f686c3fc2eeb
Xiang511 <toby91511@gmail.com>
2025-12-14 08:19:45 +0800

    fix: 禁用 CORS 中間件

--- server/src/index.js
+++ server/src/index.js
@@ -38,7 +38,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/javascripts', express.static(path.join(__dirname, 'javascripts')));
 app.set('view engine', 'ejs'); // 設置 EJS 為視圖引擎
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
